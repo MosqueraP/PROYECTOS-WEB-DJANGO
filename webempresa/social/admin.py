d3f13af8d7734aa60787d500ca7f2e5ab3c101a2
Plinio <mcplinio.15@gmail.com>
2024-01-22 18:26:12 -0500

    personalizar panel Admin

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from social.models import Link
 
 # Register your models here.
+
+class LinkAmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'update') # campos de solo lectura para el admin
+
+admin.site.register(Link, LinkAmin)
