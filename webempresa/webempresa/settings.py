c720d0bf1da5822bed7d83b62ef91d3ed21ac67d
Plinio <mcplinio.15@gmail.com>
2024-01-22 01:50:43 -0500

    configurar extendida en services/apps.py para identificar la app con nombre propio

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "core",
-    "services",
+    # ServicesConfig
+    # compiguracion extendida para el nombre de la aplicacion en el panel
+    "services.apps.ServicesConfig", 
 ]
 
 MIDDLEWARE = [
