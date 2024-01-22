5e3db93e930ba424018aba0f182963429d490e31
Plinio <mcplinio.15@gmail.com>
2024-01-21 23:58:21 -0500

    organizar mejor las rutas URLS utilizando include

--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -8,17 +8,20 @@ def home(request):
     return HttpResponse('Inicio')
 
 def about(request):
-    return HttpResponse('Historia')
+    return HttpResponse('Acerca de')
 
 def services(request):
     return HttpResponse('Servicios')
 
 def store(request):
-    return HttpResponse('Visítanos')
+    return HttpResponse('store')
 
 def contact(request):
     return HttpResponse('Contacto')
 
 def blog(request):
     return HttpResponse('Blog')
+
+def sample(request):
+    return HttpResponse('Sample')
 
