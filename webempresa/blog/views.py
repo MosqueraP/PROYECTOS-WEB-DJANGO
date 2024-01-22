3da65e0cd38e0486e345f4d3b68bf349e2def46e
Plinio <mcplinio.15@gmail.com>
2024-01-22 11:05:41 -0500

    mostar los datos de la base de datos en la vista de blog utilizando views y templates

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    post = Post.objects.all()
+    return render(request, 'blog/blog.html', {'post': post})
