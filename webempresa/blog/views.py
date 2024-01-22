55dc744af08ad37407b2ce3b38c6951f1d09dbad
Plinio <mcplinio.15@gmail.com>
2024-01-22 17:29:49 -0500

    Mostrar las categorias en el pie de la pagina

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -7,8 +7,8 @@ def blog(request):
     post = Post.objects.all()
     return render(request, 'blog/blog.html', {'post': post})
 
+
 def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id) #filtro buscar
     posts = Post.objects.filter(categories=category)
-    category = Category.objects.get(id=category_id)
-    return render(request, "blog/category.html", 
-        {'category':category, 'posts':posts})
+    return render(request, "blog/category.html", {'posts':posts, 'posts':posts})
