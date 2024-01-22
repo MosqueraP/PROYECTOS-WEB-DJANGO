da2c08dc6b2f1fbb9e843ad4b99073e56815b037
Plinio <mcplinio.15@gmail.com>
2024-01-22 01:46:42 -0500

    crear modelos de la app de servivios, registar modelos en el admin, colocar campos (created y update) de solo lectura en el admin

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "core",
+    "services",
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 
 TIME_ZONE = "UTC"
 
