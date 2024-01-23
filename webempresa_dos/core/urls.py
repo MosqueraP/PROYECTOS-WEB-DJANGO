1f43935234c5e53281877093507a24b6141e0570
Plinio <mcplinio.15@gmail.com>
2024-01-23 18:45:10 -0500

    fusionar vistas en funciones a CBV vistas de clases, genericas que devuelven solo templetes  TemplateView

--- webempresa_dos/core/urls.py
+++ webempresa_dos/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from core.views import HomePageView, SampleView
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('sample/', views.sample, name="sample"),
+    path("", HomePageView.as_view(), name="home"),
+    path("", SampleView.as_view(), name="sample"),
 ]
