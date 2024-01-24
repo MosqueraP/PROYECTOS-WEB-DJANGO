bdebfbb619702857a91c65af78587037279bd7fc
Plinio <mcplinio.15@gmail.com>
2024-01-24 00:37:30 -0500

    actualizaacion de paginas con UpdateView, y editar vistas en templates

--- webempresa_dos/pages/views.py
+++ webempresa_dos/pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from pages.models import Page
 
@@ -17,5 +17,14 @@ class PageDetailView(DetailView):
 # Vista encargada de crear paginas
 class PageCreateView(CreateView):
     model = Page
-    fields = ['title', 'content', 'order'] # campos a llenar
+    fields = ['title', 'content', 'order'] # campos a llenar y crear paginas
     success_url = reverse_lazy('pages:pages')
+
+# Actualizar o editar paginas
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order'] # campos a llenar y editar paginas
+    template_name_suffix = "_update_form"
+    
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id])
