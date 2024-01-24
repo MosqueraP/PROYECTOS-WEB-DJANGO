d44f5db812f136a785272a1fe981f93469cbddbe
Plinio <mcplinio.15@gmail.com>
2024-01-24 01:59:18 -0500

    añadir foormulario a la vista crear, colcoar estilos en en el fomulario de ckaeditor y hacerlo adactativo en todas la vistas al igual que en admin

--- webempresa_dos/pages/forms.py
+++ webempresa_dos/pages/forms.py
@@ -8,7 +8,7 @@ class PageForm(forms.ModelForm):
         fields = ['title', 'content', 'order']
         widgets = {
             'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Título'}),
-            'content': forms.Textarea(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Contenido'}),
             'order': forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Orden  '}),
         }
         labels = {
