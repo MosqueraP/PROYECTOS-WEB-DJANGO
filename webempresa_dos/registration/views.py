e2f340cc9b5bb1115f063bb91e51826e47790407
Plinio <mcplinio.15@gmail.com>
2024-01-25 03:39:29 -0500

    Formulario de registro funcional Registro con redireccion de ususarios a vistas indicadas

--- webempresa_dos/registration/views.py
+++ webempresa_dos/registration/views.py
@@ -7,5 +7,7 @@ from django.urls import reverse_lazy
 # vista de registro ususarios
 class SignUpView(CreateView):
     form_class = UserCreationForm 
-    success_url = reverse_lazy('login') # redireccionar a login a login
     template_name = 'registration/signup.html' #carag el template en esta ubicación
+
+    def get_success_url(self):
+        return reverse_lazy('login') + '?register' # redireccionar a login a login
