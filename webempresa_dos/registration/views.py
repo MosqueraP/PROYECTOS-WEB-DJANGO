94eb9613a68f9eab0e6a8a0b0412e7d95bd2176d
Plinio <mcplinio.15@gmail.com>
2024-01-25 03:25:56 -0500

    aregar eimplementar registros de ususarios, signup Funcionalidade para registrar ususarios

--- webempresa_dos/registration/views.py
+++ webempresa_dos/registration/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm # formulario generico
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+# vista de registro ususarios
+class SignUpView(CreateView):
+    form_class = UserCreationForm 
+    success_url = reverse_lazy('login') # redireccionar a login a login
+    template_name = 'registration/signup.html' #carag el template en esta ubicación
