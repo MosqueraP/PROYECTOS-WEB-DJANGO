36cee216d7172c054598f6b80be5c7b302416088
Plinio <mcplinio.15@gmail.com>
2024-01-25 03:57:13 -0500

    implementar wides en campos de los fiormularios informativo para los usuarios al registrarse

--- webempresa_dos/registration/views.py
+++ webempresa_dos/registration/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm # formulario generico
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django import forms
 
 # Create your views here.
 
@@ -11,3 +12,15 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register' # redireccionar a login a login
+    
+    def get_form(self, form_class=None):
+        form = super(SignUpView, self).get_form()
+        # modificar en tiempo real los widges
+        form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder':'Nombre de ususario'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':'Repite la contraseña'})
+        # form.fields['username'].label = '' #quita las label
+        return form
+    
+    
+
