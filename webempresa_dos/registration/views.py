bb386914fb4ad90fbb29ece732fa39ee3974be38
Plinio <mcplinio.15@gmail.com>
2024-01-25 04:30:02 -0500

    extender el formulario de registro con campo email formulario funcionando bien y alamcenando usuarios con conctraseña y validaciones

--- webempresa_dos/registration/views.py
+++ webempresa_dos/registration/views.py
@@ -1,3 +1,4 @@
+from registration.forms import UserCreationFormWithEmail
 from django.contrib.auth.forms import UserCreationForm # formulario generico
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -7,7 +8,8 @@ from django import forms
 
 # vista de registro ususarios
 class SignUpView(CreateView):
-    form_class = UserCreationForm 
+    # form_class = UserCreationForm # quitamos el formulario generico
+    form_class = UserCreationFormWithEmail # formulario extencion desde registration.form.py
     template_name = 'registration/signup.html' #carag el template en esta ubicación
 
     def get_success_url(self):
@@ -17,6 +19,7 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
         # modificar en tiempo real los widges
         form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder':'Nombre de ususario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control mb-2', 'placeholder':'Direccion email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':'Repite la contraseña'})
         # form.fields['username'].label = '' #quita las label
