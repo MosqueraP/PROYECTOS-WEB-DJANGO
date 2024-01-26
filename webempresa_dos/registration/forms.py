5ff47bc526ef6d01fd45a048df7534096e4b905c
Plinio <mcplinio.15@gmail.com>
2024-01-25 20:18:44 -0500

    editar el email del ususario y adacta las vistas, rutas y vistas

--- webempresa_dos/registration/forms.py
+++ webempresa_dos/registration/forms.py
@@ -29,3 +29,20 @@ class ProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 3, 'placeholder': 'Biografía'}),
             'link': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enlace'}), 
         }
+
+# formulario para editar el email dentro del perfil
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text='Requerido, 254 caracteres como maximo y debe ser valido.')
+    class Meta:
+        model = User
+        fields = ['email'] # campo a editar
+        # widgets = {
+        #     'email': forms.EmailInput(attrs={'class': 'form-control-file mt-3'}),
+        #     }    
+
+    def clean_email(self): # validacion que el email sea unico
+            email= self.cleaned_data.get("email")
+            if 'email' in self.changed_data: # verificar si este email se encuentra en la data
+                if User.objects.filter(email=email).exists():
+                    raise forms.ValidationError('El email ya está registrado, prueba con otro')
+            return email
--- webempresa_dos/registration/views.py
+++ webempresa_dos/registration/views.py
@@ -1,7 +1,6 @@
-from registration.forms import ProfileForm
+from registration.forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
-from registration.forms import UserCreationFormWithEmail
 from django.contrib.auth.forms import UserCreationForm # formulario generico
 from django.views.generic import CreateView, TemplateView
 from django.views.generic.edit import UpdateView
@@ -42,3 +41,21 @@ class ProfileUpdate(UpdateView):
     def get_object(self):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
+    
+# vista para editar el email dentro del perfil
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile') # al editar volver al perfil
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        # recuperar el objeto que se va editar        
+        return self.request.user
+
+    def get_form(self, form_clas=None):
+        # widge email
+        form = super(EmailUpdate, self).get_form()
+        # modificar en tiempo real los widges
+        form.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control mb-2', 'placeholder':'Email'})
+        return form
